a8e61a33f365a7bcb2fb6adfbb94958d96fdb91f
hamit <hamid@hamit-IT.(none)>
2013-06-06 20:27:51 +1000

    Add all files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JobScheduling::Application.config.secret_token = '7c069ef76a8477a6dcf8c5aff28268d7e2f89342d980ea10205e2a7efa1908a6307af14ff7de009c226817d0383b5bd3dda3b2979aec95343cf01693c28eb122'
